a4a69ae0d8982a6917894f6ca67e10a2df351b0b
sgpl <sharad45@gmail.com>
2017-08-10 17:10:41 -0400

    Made entertainment_center.py fully pep8 compliant

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,8 @@
 import fresh_tomatoes
 import media
 
+
+# the following is an instance of class movie
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
@@ -9,6 +11,8 @@ toy_story = media.Movie("Toy Story",
                         "1995",
                         "John Lasseter")
 
+
+# the following is an instance of class movie
 avatar = media.Movie("Avatar",
                      "A story about aliens",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
@@ -18,6 +22,7 @@ avatar = media.Movie("Avatar",
                      "James Cameron")
 
 
+# the following is an instance of class movie
 the_godfather = media.Movie("The Godfather",
                             "A story about hope, and cool things",
                             "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",  # NOQA
@@ -27,40 +32,60 @@ the_godfather = media.Movie("The Godfather",
                             "Francis F. Coppola")
 
 
+# the following is an instance of class movie
 inception = media.Movie("Inception",
-                        "The film tells the story of an alternate reality where people can invade your dreams and plant ideas.",
+                        "The film tells the story of an alternate reality"
+                        " where people can invade your dreams"
+                        " and plant ideas.",
                         "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",  # NOQA
                         "https://youtu.be/66TuSJo4dZM",
-                        "148 Minutes",                         "2010",
+                        "148 Minutes",
+                        "2010",
                         "Christopher Nolan")
 
 
+# the following is an instance of class movie
 wonder_woman = media.Movie("Wonder Woman",
-                           "A story about wonder woman and her contribution during WWII",
+                           "A story about wonder woman and her contribution "
+                           "during WWII",
                            "http://www.dccomics.com/sites/default/files/imce/2016/07-JUL/WWPosterRdcd_579260f4b9aba4.81209460.jpg",  # NOQA
                            "https://youtu.be/VSB4wGIdDwo",
                            "141 Minutes",
                            "2017",
                            "Patty Jenkins")
 
+
+# the following is an instance of class movie
 casino_royale = media.Movie("Casino Royale",
-                            "Casino Royale is set at the beginning of Bond's career as Agent 007, just as he is earning his licence to kill.",
+                            "Casino Royale is set at the beginning of Bond's "
+                            "career as Agent 007, just as he is earning"
+                            " his licence to kill.",
                             "https://upload.wikimedia.org/wikipedia/en/1/15/Casino_Royale_2_-_UK_cinema_poster.jpg",  # NOQA
                             "https://youtu.be/36mnx8dBbGE",
                             "144 Minutes",
                             "2006",
                             "Martin Campbell")
 
+
+# the following is an instance of class movie
 dark_knight = media.Movie("The Dark Knight",
-                          "Dark, complex and unforgettable, The Dark Knight succeeds not just as an entertaining comic book film, but as a richly thrilling crime saga.",
+                          "Dark, complex and unforgettable, The Dark Knight "
+                          "succeeds not just as an entertaining comic book"
+                          " film, but as a richly thrilling crime saga.",
                           "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",  # NOQA
                           "https://youtu.be/EXeTwQWrcwY",
                           "152 Minutes",
                           "2008",
                           "Christopher Nolan")
 
+
+# the following is an instance of class movie
 angry_men = media.Movie("12 Angry Men",
-                        "A jury argues a case in a stuffy room on a hot summer's day. Eleven say 'guilty!'' But one holdout (Jack Lemmon) is convinced of the defendant's innocence and stubbornly argues reasonable doubt.",
+                        "A jury argues a case in a stuffy room on a"
+                        " hot summer's day. Eleven say 'guilty!' But"
+                        " one holdout (Jack Lemmon) is convinced of"
+                        " the defendant's innocence and "
+                        "stubbornly argues reasonable doubt.",
                         "https://upload.wikimedia.org/wikipedia/en/4/44/12_Angry_Men_1997_film_poster.jpg",  # NOQA
                         "https://youtu.be/A7CBKT0PWFA",
                         "117 Minutes",
@@ -68,16 +93,22 @@ angry_men = media.Movie("12 Angry Men",
                         "William Friedkin")
 
 
+# the following is an instance of class movie
 slist = media.Movie("Schindler's List",
-                    "Schindler's List blends the abject horror of the Holocaust with Steven Spielberg's signature tender humanism to create the director's dramatic masterpiece",
+                    "Schindler's List blends the abject horror of the "
+                    "Holocaust with Steven Spielberg's signature tender "
+                    "humanism to create the director's dramatic masterpiece",
                     "https://upload.wikimedia.org/wikipedia/en/3/38/Schindler%27s_List_movie.jpg",  # NOQA
                     "https://youtu.be/JdRGC-w9syA",
                     "196 Minutes",
                     "1993",
                     "Steven Spielberg")
 
+
+# the following is an instance of class movie
 ant_man = media.Movie("Ant Man",
-                      "The story about a man who becomes an ant, and harnesses an ant's super powers to save the world",
+                      "The story about a man who becomes an ant, and harnesses"
+                      " an ant's super powers to save the world",
                       "https://upload.wikimedia.org/wikipedia/en/7/75/Ant-Man_poster.jpg",  # NOQA
                       "https://youtu.be/pWdKf3MneyI",
                       "117 Minutes",
@@ -85,6 +116,7 @@ ant_man = media.Movie("Ant Man",
                       "Peyton Reed")
 
 
+# the following is an instance of class movie
 forrestgump = media.Movie("Forrest Gump",
                           "A movie about hope, determination and beliefs.",
                           "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",  # NOQA
@@ -93,6 +125,8 @@ forrestgump = media.Movie("Forrest Gump",
                           "1994",
                           "Robert Zemeckis")
 
+
+# the following is an instance of class movie
 matrix = media.Movie("The Matrix",
                      "A movie about an interesting alternate reality!",
                      "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # NOQA
@@ -102,5 +136,11 @@ matrix = media.Movie("The Matrix",
                      "Wachowski Brothers")
 
 
-movies = [dark_knight, the_godfather, angry_men, wonder_woman, inception, toy_story, casino_royale, ant_man, slist, avatar, matrix, forrestgump]
+# the following is a list that contains the instances of
+# the movie class
+movies = [dark_knight, the_godfather, angry_men,
+          wonder_woman, inception, toy_story, casino_royale,
+          ant_man, slist, avatar, matrix, forrestgump]
+
+# open the HTML file in a webbrowser via the fresh_tomatoes.py file
 fresh_tomatoes.open_movies_page(movies)
